7aeabb60d50aaa16cf34c3f8ba18ee4603f993b5
Rohith Poyyeri <rohithart@yahoo.com>
2022-05-08 20:05:37 +1000

    Include Angular Universal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { AboutModule } from './components/about/about.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
